5edcecf51cd720045d9c7f4eb2cb2c74f8151860
paraxels <entwinedlines@proton.me>
2025-06-25 16:31:25 -0600

    fix vercel preview env X-Frame-Options: Deny

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,28 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+
+  async headers() {
+    const isPreview = process.env.VERCEL_ENV === 'preview';
+
+    return isPreview
+      ? [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'X-Frame-Options',
+                value: 'ALLOWALL',
+              },
+              {
+                key: 'Content-Security-Policy',
+                value: 'frame-ancestors *',
+              },
+            ],
+          },
+        ]
+      : [];
+  },
 };
 
 export default nextConfig;
